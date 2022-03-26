8c5c35c40cbe67b75f9cc1e43d70649b74215901
moodee18 <102429339+moodee18@users.noreply.github.com>
2022-03-26 15:53:09 -0600

    Update enviar.php

--- css/enviar.php
+++ css/enviar.php
@@ -3,7 +3,7 @@ $destino="moodeedelacruz@gmail.com"
 $nombre=$_POST["nombre"]
 $correo=$_POST["correo"]
 $mensaje= $_POST["mensaje"]
-$contenido="nombre:". $nombre .   "\ncorreo:" .   $correo . "nmensaje."  .   $mensaje      
+$contenido="nombre:". $nombre .   "\ncorreo:" .   $correo . "\nmensaje."  .   $mensaje      
 mail[$destino, "contacto", $contenido];
 echo´<script>
     alert("su correo se envió correctamente")
